4e875092e378e35b1c90811663c235a1098005df
Sai Shanker Lakkavajjala <lsaishanker@yahoo.com>
2016-08-21 14:57:59 -0400

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,31 @@
+library(data.table)
+
+readDataSet<-function()
+{
+    graphDataSet<-read.table("household_power_consumption.txt",
+                             sep = ";",
+                             na.strings = "?",
+                             header = TRUE)
+    selectDataSet<-graphDataSet[graphDataSet$Date %in% c("1/2/2007","2/2/2007"),]
+    selectDataSet[,1]<-as.Date(selectDataSet[,1],format="%d/%m/%Y")
+    mergedDateTime<-as.data.frame(paste(selectDataSet[,1],selectDataSet[,2],sep = " "))
+    mergedDateTime[,1]<-as.data.frame(strptime(mergedDateTime[,1],format="%Y-%m-%d %H:%M:%S"))
+    names(mergedDateTime)<-c("DateTime")
+    
+    mergedDS<-cbind(mergedDateTime,selectDataSet[,-(1:2)])
+
+    mergedDS
+}
+
+plot1<-function()
+{
+    graphDS<-readDataSet()
+    png(file="plot1.png",height=480,width = 480)
+    hist(graphDS$Global_active_power,
+         col = "red",
+         xlab = "Global Active Power (Kilowatts)",
+         main = "Global Active Power"
+    )
+    dev.off()
+    
+}
